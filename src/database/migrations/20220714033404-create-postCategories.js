2c444587e68eab41fe91250d458eb4633b2930e1
André Alves <andreluialves@hotmail.com>
2022-07-14 04:05:35 +0000

    feat: criadas as migrations para as entidades user, categories, blogPosts, postCategories - req01

---
+++ src/database/migrations/20220714033404-create-postCategories.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        // field: 'post_id',
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        }
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        // field: 'category_id',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        }
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
