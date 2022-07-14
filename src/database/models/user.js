79989ed96f23af9038fb3d918e27c9c8234e2cb6
André Alves <andreluialves@hotmail.com>
2022-07-14 04:20:08 +0000

    feat: criada a model User em src/database/models/user.js - req.02

---
+++ src/database/models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = User;
