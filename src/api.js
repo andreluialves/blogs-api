e6ca055efdbd2ba6dee825ab67b18d0cd8121858
André Alves <andreluialves@hotmail.com>
2022-07-14 19:32:04 -0300

    feat: configuração do jwt e criacao do endpoint configuracao do jwt e criação do endpoint /login

--- src/api.js
+++ src/api.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const authRouter = require('./routers/authRouter');
+const authController = require('./controllers/authController');
+const errorMiddleware = require('./middlewares/errorMiddleware')
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', authRouter);
+
+app.use(authController.validateToken);
+
+app.use(errorMiddleware);
 
 // ...
 
