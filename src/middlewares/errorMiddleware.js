e4eaf4f6f177c0b361de1bc0a174c42c4ac8cf59
André Alves <andreluialves@hotmail.com>
2022-07-18 01:08:48 -0300

    refactor: correcao de status de erro

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -11,7 +11,7 @@ module.exports = (err, _req, res, _next) => {
         res.status(409).json({ message });
         break;
       case 'UnauthorizedError':
-        res.status(400).json({ message });
+        res.status(401).json({ message });
         break;
       default:
         res.status(500).json({ message });
