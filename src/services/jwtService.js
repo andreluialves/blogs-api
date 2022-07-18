bb0e439ce946424a37f9b9e0d7a708a7b1cbaceb
André Alves <andreluialves@hotmail.com>
2022-07-18 01:10:16 -0300

    feat: criacao dos tipos de erro da funcao de validacao do token

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -6,6 +6,22 @@ const jwtService = {
     const token = jwt.sign({ data }, process.env.JWT_SECRET);
     return token;
   },
+
+  validateToken: (token) => {
+    try {
+      const data = jwt.verify(token, process.env.JWT_SECRET);
+      return data;
+    } catch (e) {
+      if (!token) {
+        const error1 = new Error('Token not found');
+        error1.name = 'UnauthorizedError';
+        throw error1; 
+      }
+      const error = new Error('Expired or invalid token');
+      error.name = 'UnauthorizedError';
+      throw error; 
+    }
+  },
 };
 
 module.exports = jwtService;
