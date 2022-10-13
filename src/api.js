6bda460e48f5cb5b0848e5c582d29316ee79436f
André Alves <andreluialves@hotmail.com>
2022-10-13 20:37:19 -0300

    style: exclusão de comentários

--- src/api.js
+++ src/api.js
@@ -1,18 +1,19 @@
 require('express-async-errors');
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
 const authRouter = require('./routers/authRouter');
 const usersRouter = require('./routers/usersRouter');
 const categoriesRouter = require('./routers/categoriesRouter');
 const blogPostsRouter = require('./routers/blogPostsRouter');
 
-// const authController = require('./controllers/authController');
 const errorMiddleware = require('./middlewares/errorMiddleware');
-
-// ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/login', authRouter);
 
@@ -22,11 +23,7 @@ app.use('/categories', categoriesRouter);
 
 app.use('/post', blogPostsRouter);
 
-// app.use(authController.validateToken);
-
 app.use(errorMiddleware);
-
-// ...
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
+
 require('dotenv').config();
 const app = require('./api');
 
-// não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // não remova esse endpoint
 app.get('/', (_request, response) => {
